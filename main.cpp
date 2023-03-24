1b652fe788fc06042aa1858bf06f0f9334ce96d7
Selisine <Aidenpike113@gmail.com>
2023-03-23 20:10:21 -0400

    Add vectors edgewoodArray and classTitles

--- main.cpp
+++ main.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
-#include <algorithm>
-
-using namespace std;
+#include <vector>
+#include <string>
 
 int main() {
-    
+    std::vector<std::string> classTitle;
+    std::vector<std::vector<std::string>> edgewoodArray;
+
+    int classAmount;
+    int assignmentAmount;
+    std::string className = " ";
+    std::string assignmentName = " ";
+
+    /*
+    Example (2 classes, 2 assignments each):
+        classTitle = {"Class 1", "Class 2"};
+        edgewoodArray = {"Class 1 Assignment 1", Class 1 Assignment 2}, {"Class 2 Assignment 1", Class 2 Assignment 2}
+        
+    classTitle[0] (Class 1)
+        edgewoodArray[0][0] (Class 1 Assignment 1)
+        edgewoodArray[0][1] (Class 1 Assignment 2)
+    classTitle[1] (Class 2)
+        edgewoodArray[1][0] (Class 2 Assignment 1)
+        edgewoodArray[1][1] (Class 2 Assignment 2)
+    */
+
+    //Get number of classes
+    std::cout << "Enter number of classes: ";
+    std::cin >> classAmount;
+    std::cin.ignore();
 }
